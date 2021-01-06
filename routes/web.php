ddedf4500845bd59e1d9c0b839314032086d0f1f
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2021-01-06 09:46:17 +0700

    Improve structure routes in web.php :art:

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\FranchiseController;
-use App\Http\Controllers\Admin\FranchiseTypeController;
-use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,36 +31,44 @@ Route::name('franchise.')->middleware('auth')->prefix('franchise')->group(functi
         $app->put('/{id}', [App\Http\Controllers\Franchise\VoucherController::class, 'update'])->name('update');
         $app->get('/{id}/delete', [App\Http\Controllers\Franchise\VoucherController::class, 'destroy'])->name('destroy');
     });
+    $app->prefix('categories')->name('category.')->group(function ($app) {
+        $app->get('/', [App\Http\Controllers\Franchise\CategoryController::class, 'index'])->name('index');
+        $app->get('/create', [App\Http\Controllers\Franchise\CategoryController::class, 'create'])->name('create');
+        $app->post('/', [App\Http\Controllers\Franchise\CategoryController::class, 'store'])->name('store');
+        $app->get('/{id}/edit', [App\Http\Controllers\Franchise\CategoryController::class, 'edit'])->name('edit');
+        $app->put('/{id}', [App\Http\Controllers\Franchise\CategoryController::class, 'update'])->name('update');
+        $app->get('/{id}/delete', [App\Http\Controllers\Franchise\CategoryController::class, 'destroy'])->name('destroy');
+    });
 });
 
 Route::name('admin.')->middleware('auth')->prefix('backoffice')->group(function ($app) {
-    $app->get('/', [DashboardController::class, 'index'])->name('dashboard');
-    $app->get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    $app->get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+    $app->get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
     $app->prefix('franchises')->name('franchise.')->group(function ($app) {
 
         $app->prefix('types')->name('type.')->group(function ($app) {
-            $app->get('/', [FranchiseTypeController::class, 'index'])->name('index');
-            $app->get('/create', [FranchiseTypeController::class, 'create'])->name('create');
-            $app->post('/', [FranchiseTypeController::class, 'store'])->name('store');
-            $app->get('/{id}/edit', [FranchiseTypeController::class, 'edit'])->name('edit');
-            $app->put('/{id}', [FranchiseTypeController::class, 'update'])->name('update');
-            $app->get('/{id}/delete', [FranchiseTypeController::class, 'destroy'])->name('destroy');
+            $app->get('/', [App\Http\Controllers\Admin\FranchiseTypeController::class, 'index'])->name('index');
+            $app->get('/create', [App\Http\Controllers\Admin\FranchiseTypeController::class, 'create'])->name('create');
+            $app->post('/', [App\Http\Controllers\Admin\FranchiseTypeController::class, 'store'])->name('store');
+            $app->get('/{id}/edit', [App\Http\Controllers\Admin\FranchiseTypeController::class, 'edit'])->name('edit');
+            $app->put('/{id}', [App\Http\Controllers\Admin\FranchiseTypeController::class, 'update'])->name('update');
+            $app->get('/{id}/delete', [App\Http\Controllers\Admin\FranchiseTypeController::class, 'destroy'])->name('destroy');
         });
 
-        $app->get('/', [FranchiseController::class, 'index'])->name('index');
-        $app->get('/create', [FranchiseController::class, 'create'])->name('create');
-        $app->post('/', [FranchiseController::class, 'store'])->name('store');
-        $app->get('/{id}/edit', [FranchiseController::class, 'edit'])->name('edit');
-        $app->put('/{id}', [FranchiseController::class, 'update'])->name('update');
-        $app->get('/{id}/delete', [FranchiseController::class, 'destroy'])->name('destroy');
+        $app->get('/', [App\Http\Controllers\Admin\FranchiseController::class, 'index'])->name('index');
+        $app->get('/create', [App\Http\Controllers\Admin\FranchiseController::class, 'create'])->name('create');
+        $app->post('/', [App\Http\Controllers\Admin\FranchiseController::class, 'store'])->name('store');
+        $app->get('/{id}/edit', [App\Http\Controllers\Admin\FranchiseController::class, 'edit'])->name('edit');
+        $app->put('/{id}', [App\Http\Controllers\Admin\FranchiseController::class, 'update'])->name('update');
+        $app->get('/{id}/delete', [App\Http\Controllers\Admin\FranchiseController::class, 'destroy'])->name('destroy');
     });
     $app->prefix('users')->name('user.')->group(function ($app) {
-        $app->get('/', [UserController::class, 'index'])->name('index');
-        $app->get('/create', [UserController::class, 'create'])->name('create');
-        $app->post('/', [UserController::class, 'store'])->name('store');
-        $app->get('/{id}/edit', [UserController::class, 'edit'])->name('edit');
-        $app->put('/{id}', [UserController::class, 'update'])->name('update');
-        $app->get('/{id}/delete', [UserController::class, 'destroy'])->name('destroy');
+        $app->get('/', [App\Http\Controllers\Admin\UserController::class, 'index'])->name('index');
+        $app->get('/create', [App\Http\Controllers\Admin\UserController::class, 'create'])->name('create');
+        $app->post('/', [App\Http\Controllers\Admin\UserController::class, 'store'])->name('store');
+        $app->get('/{id}/edit', [App\Http\Controllers\Admin\UserController::class, 'edit'])->name('edit');
+        $app->put('/{id}', [App\Http\Controllers\Admin\UserController::class, 'update'])->name('update');
+        $app->get('/{id}/delete', [App\Http\Controllers\Admin\UserController::class, 'destroy'])->name('destroy');
     });
 });
 
