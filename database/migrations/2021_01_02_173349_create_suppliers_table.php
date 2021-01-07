4e1f769f6b10e4fc11cfba981c0d0b598a23054b
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2021-01-07 11:34:03 +0700

    Fix typo in model supplier :pencil2:

--- database/migrations/2021_01_02_173349_create_suppliers_table.php
+++ database/migrations/2021_01_02_173349_create_suppliers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSupliersTable extends Migration
+class CreateSuppliersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSupliersTable extends Migration
      */
     public function up()
     {
-        Schema::create('supliers', function (Blueprint $table) {
+        Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('address');
@@ -30,6 +30,6 @@ class CreateSupliersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('supliers');
+        Schema::dropIfExists('suppliers');
     }
 }
