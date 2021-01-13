facd7cbad9f38ba729969e1831ba80ef75effc1b
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2021-01-14 06:16:12 +0700

    :sparkles: Application : Improve sidebar

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,16 +1,8 @@
 <footer class="footer">
     <div class="row">
         <div class="col-md-6 col-sm-12">
-            <p>Pampered by<a href="#" class="text-dark">Hencework</a> © 2019</p>
+            <p>ASTA APPLICATION &copy; 2020</p>
         </div>
-        <div class="col-md-6 col-sm-12">
-            <p class="d-inline-block">Follow us</p>
-            <a href="#" class="d-inline-block btn btn-icon btn-icon-only btn-indigo btn-icon-style-4"><span
-                    class="btn-icon-wrap"><i class="fa fa-facebook"></i></span></a>
-            <a href="#" class="d-inline-block btn btn-icon btn-icon-only btn-indigo btn-icon-style-4"><span
-                    class="btn-icon-wrap"><i class="fa fa-twitter"></i></span></a>
-            <a href="#" class="d-inline-block btn btn-icon btn-icon-only btn-indigo btn-icon-style-4"><span
-                    class="btn-icon-wrap"><i class="fa fa-google-plus"></i></span></a>
-        </div>
+
     </div>
 </footer>
--- resources/views/partials/franchise-sidebar.blade.php
+++ resources/views/partials/franchise-sidebar.blade.php
@@ -22,54 +22,120 @@
                 <ul id="auth_drp" class="nav flex-column collapse collapse-level-1">
                     <li class="nav-item">
                         <ul class="nav flex-column">
-                            <li
-                                class="nav-item {{ set_active(['franchise.voucher.index','franchise.voucher.create','franchise.voucher.edit']) }}">
-                                <a class="nav-link" href="{{ route('franchise.voucher.index') }}">Voucher</a>
+                            <li class="nav-item">
+                                <a class="nav-link" href="javascript:void(0);" data-toggle="collapse"
+                                    data-target="#order_drp">
+                                    Order
+                                </a>
+                                <ul id="order_drp" class="nav flex-column collapse collapse-level-2">
+                                    <li class="nav-item">
+                                        <ul class="nav flex-column">
+                                            <li
+                                                class="nav-item {{ set_active(['franchise.voucher.index','franchise.voucher.create','franchise.voucher.edit']) }}">
+                                                <a class="nav-link"
+                                                    href="{{ route('franchise.voucher.index') }}">Voucher</a>
+                                            </li>
+                                            <li class="nav-item {{ set_active(['franchise.order.index']) }}">
+                                                <a class="nav-link"
+                                                    href="{{ route('franchise.order.index') }}">Order</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
                             </li>
-                            <li class="nav-item {{ set_active(['franchise.order.index']) }}">
-                                <a class="nav-link" href="{{ route('franchise.order.index') }}">Order</a>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="javascript:void(0);" data-toggle="collapse"
+                                    data-target="#product_drp">
+                                    Product
+                                </a>
+                                <ul id="product_drp" class="nav flex-column collapse collapse-level-2">
+                                    <li class="nav-item">
+                                        <ul class="nav flex-column">
+                                            <li
+                                                class="nav-item {{ set_active(['franchise.category.index','franchise.category.create','franchise.category.edit']) }}">
+                                                <a class="nav-link"
+                                                    href="{{ route('franchise.category.index') }}">Category</a>
+                                            </li>
+                                            <li
+                                                class="nav-item {{ set_active(['franchise.product.index','franchise.product.create','franchise.product.edit']) }}">
+                                                <a class="nav-link"
+                                                    href="{{ route('franchise.product.index') }}">Product</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
                             </li>
-                            <li
-                                class="nav-item {{ set_active(['franchise.category.index','franchise.category.create','franchise.category.edit']) }}">
-                                <a class="nav-link" href="{{ route('franchise.category.index') }}">Category</a>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="javascript:void(0);" data-toggle="collapse"
+                                    data-target="#stock_drp">
+                                    Stock
+                                </a>
+                                <ul id="stock_drp" class="nav flex-column collapse collapse-level-2">
+                                    <li class="nav-item">
+                                        <ul class="nav flex-column">
+
+                                            <li
+                                                class="nav-item {{ set_active(['franchise.supplier.index','franchise.supplier.create','franchise.supplier.edit']) }}">
+                                                <a class="nav-link"
+                                                    href="{{ route('franchise.supplier.index') }}">Supplier</a>
+                                            </li>
+                                            <li
+                                                class="nav-item {{ set_active(['franchise.material.index','franchise.material.create','franchise.material.edit']) }}">
+                                                <a class="nav-link"
+                                                    href="{{ route('franchise.material.index') }}">Material</a>
+                                            </li>
+
+                                        </ul>
+                                    </li>
+                                </ul>
                             </li>
-                            <li
-                                class="nav-item {{ set_active(['franchise.supplier.index','franchise.supplier.create','franchise.supplier.edit']) }}">
-                                <a class="nav-link" href="{{ route('franchise.supplier.index') }}">Supplier</a>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="javascript:void(0);" data-toggle="collapse"
+                                    data-target="#employee_drp">
+                                    Employee
+                                </a>
+                                <ul id="employee_drp" class="nav flex-column collapse collapse-level-2">
+                                    <li class="nav-item">
+                                        <ul class="nav flex-column">
+
+
+
+                                            <li
+                                                class="nav-item {{ set_active(['franchise.employee.index','franchise.employee.create','franchise.employee.edit']) }}">
+                                                <a class="nav-link"
+                                                    href="{{ route('franchise.employee.index') }}">Employee</a>
+                                            </li>
+                                            <li
+                                                class="nav-item {{ set_active(['franchise.employee.salary.index','franchise.employee.salary.create','franchise.employee.salary.edit']) }}">
+                                                <a class="nav-link"
+                                                    href="{{ route('franchise.employee.salary.index') }}">Employee
+                                                    Salary</a>
+                                        </ul>
+                                    </li>
+                                </ul>
                             </li>
-                            <li
-                                class="nav-item {{ set_active(['franchise.material.index','franchise.material.create','franchise.material.edit']) }}">
-                                <a class="nav-link" href="{{ route('franchise.material.index') }}">Material</a>
-                            </li>
-                            <li
-                                class="nav-item {{ set_active(['franchise.product.index','franchise.product.create','franchise.product.edit']) }}">
-                                <a class="nav-link" href="{{ route('franchise.product.index') }}">Product</a>
-                            </li>
-                            <li
-                                class="nav-item {{ set_active(['franchise.employee.index','franchise.employee.create','franchise.employee.edit']) }}">
-                                <a class="nav-link" href="{{ route('franchise.employee.index') }}">Employee</a>
-                            </li>
-                            <li
-                                class="nav-item {{ set_active(['franchise.employee.salary.index','franchise.employee.salary.create','franchise.employee.salary.edit']) }}">
-                                <a class="nav-link" href="{{ route('franchise.employee.salary.index') }}">Employee
-                                    Salary</a>
-                            </li>
-                        </ul>
+
+
                     </li>
                 </ul>
             </li>
-            <li class="nav-item {{ set_active('franchise.report.index') }}">
-                <a class="nav-link" href="{{ route('franchise.report.index') }}">
-                    <i class="ion ion-ios-apps"></i>
-                    <span class="nav-link-text">Report</span>
-                </a>
-            </li>
-            <li class="nav-item {{ set_active('franchise.setting.index') }}">
-                <a class="nav-link" href="{{ route('franchise.setting.index') }}">
-                    <i class="ion ion-ios-settings"></i>
-                    <span class="nav-link-text">Account Setting</span>
-                </a>
-            </li>
+        </ul>
+        </li>
+        <li class="nav-item {{ set_active('franchise.report.index') }}">
+            <a class="nav-link" href="{{ route('franchise.report.index') }}">
+                <i class="ion ion-ios-apps"></i>
+                <span class="nav-link-text">Report</span>
+            </a>
+        </li>
+        <li class="nav-item {{ set_active('franchise.setting.index') }}">
+            <a class="nav-link" href="{{ route('franchise.setting.index') }}">
+                <i class="ion ion-ios-settings"></i>
+                <span class="nav-link-text">Account Setting</span>
+            </a>
+        </li>
         </ul>
     </div>
 </div>
