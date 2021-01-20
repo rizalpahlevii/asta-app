e049216391ae4f6711a3b503c7060b10b1402720
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2021-01-20 23:50:45 +0700

    improve bug

--- resources/views/pages/admin/user/index.blade.php
+++ resources/views/pages/admin/user/index.blade.php
@@ -71,6 +71,7 @@
     rel="stylesheet" type="text/css" />
 
 @endpush
+@push('script')
 
 <script src="{{ asset('admin_template') }}/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="{{ asset('admin_template') }}/vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
